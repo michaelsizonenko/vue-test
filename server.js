0614a5206b2af0debf0247695fc4da256a1ad8ef
michaelsizonenko <michael.sizonenko.17@gmail.com>
2021-09-10 16:19:03 +0300

    Test 2

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
+let servestatic = require('serve-static')
+let path = require('path')
+let express = require('express')
+let port = process.env.PORT || 3000;
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
-app.listen(port);
-
+app.use(servestatic(path.join(path.resolve(), 'dist')));
+app.listen(port, () => {console.log("API server started on "+app.get('port'));});
