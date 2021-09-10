8e8392dde35116d3eebc6ebf8d9c08ea67b9294b
michaelsizonenko <michael.sizonenko.17@gmail.com>
2021-09-10 16:00:28 +0300

    Test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <h1 STYLE="color: #339170; padding: 10%">ZEN TEST</h1>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
